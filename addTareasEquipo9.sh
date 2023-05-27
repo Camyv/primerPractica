f82a255e078f12a073c15c468968d1bde8c52e20
Camila Vicente <camyvic03@gmail.com>
2023-05-27 19:08:17 -0300

    guardado de TXT y SH

---
+++ addTareasEquipo9.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo "¿Tenes tareas?"
+read -p "Ingrese su tarea:" tarea
+echo $tarea >> TareasDiarias.txt
+echo "Su tarea se agrego correctamente a TareasDiarias.txt'."[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[t[D"tareasDiarias.txt[C[C
